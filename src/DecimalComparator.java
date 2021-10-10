a860a3bcf86a9f9cd22c9713b159e04c9c9dea2f
Rohhian <kristianmiilen@gmail.com>
2021-10-10 16:21:26 +0300

    Initial commit

---
+++ src/DecimalComparator.java
@@ -0,0 +1,5 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double numOne, double numTwo) {
+        return (int) (numOne * 1000) == (int) (numTwo * 1000);
+    }
+}
